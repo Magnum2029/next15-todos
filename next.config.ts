6699949d663e8bcd0e9fc3dc1029e1b6d8809a66
Magnum2029 <magnumsantos97@gmail.com>
2025-09-04 16:41:58 +0100

    a

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,11 @@
-// next.config.ts
 import type { NextConfig } from 'next';
 
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  // gera site 100% estático para o GitHub Pages
   output: 'export',
-
-  // base do projeto (IMPORTANTÍSSIMO p/ project pages)
   basePath: isProd ? '/next15-todos' : '',
-
-  // garante que os assets (CSS/JS) carreguem no GitHub Pages
   assetPrefix: isProd ? '/next15-todos/' : undefined,
-
-  // ajuda o Pages a encontrar index.html nas pastas
   trailingSlash: true,
 };
 
