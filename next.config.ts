45029e0e2978782a5f86ff8cba7d3cd211f7ca68
Magnum2029 <magnumsantos97@gmail.com>
2025-09-07 18:42:51 +0100

    Export estático para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
+const repo = "next15-todos";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? `/${repo}` : undefined,
+  assetPrefix: isProd ? `/${repo}/` : undefined
 };
 
 export default nextConfig;
